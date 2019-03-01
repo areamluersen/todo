f4cd26226fd36546565042b8391297e8baafdbea
Aream Thomas Luersen <aluersen@geomais.com.br>
2019-03-01 11:00:50 -0300

    todo list started

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: [],
+    }
+  }
+  
+  handleClickAdd(task) {
+    const todo = this.state.todo;
+    this.setState({todo: todo.concat([task])})
+    //console.log(todo);
+  }
+
+  render(){
+    const todo = this.state.todo;
+    const tasks = todo.map((task, i) => {
+      return(
+        <li key={i}>
+          <p>{task}</p>
+        </li>
+      )
+    });
+
+    return(
+    <div>
+      <div>
+      <button className="todo" onClick={() => this.handleClickAdd('Clicou')}>
+        {'Adicionar tarefa'} 
+      </button>
       </div>
-    );
+      <div>
+        <ol>{tasks}</ol>
+      </div>
+      
+    </div>
+    )
   }
 }
 
